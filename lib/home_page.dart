8918413199b23397e5ae4cbe7ded0b73574ce919
Fiza Hussain <hussainfiza5@gmail.com>
2023-06-04 08:13:57 +0500

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Fiza :)";
+    bool isMale = false;
+    num temp = 30.5; //can take int or double
+    var day = 5;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ), //head
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
